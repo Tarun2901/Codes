c035ed10b479932354f5f9546c914c0a9b8e9eb8
Tarun Yadav <tarunrock2901@gmail.com>
2020-05-21 20:39:07 +0530

    Add files via upload

---
+++ linkedlist.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+#include "Node.cpp"
+
+void print(Node *head)
+{Node *temp=head;
+while(temp !=NULL)
+{
+cout<<temp->data<<"->";
+temp=temp->next;
+}
+temp=head;
+while(temp !=NULL)
+{
+cout<<temp->data<<"->";
+temp=temp->next;
+}
+}
+
+int main()
+{//Statically 
+Node n1(1);
+Node *head= &n1;
+Node n2(2);
+Node n3(3);
+Node n4(4);
+Node n5(5);
+n1.next=&n2;
+n2.next=&n3;
+n3.next=&n4;
+n4.next=&n5;
+
+print(head);
+/*
+n1.next=&n2;
+cout<<head->data<<" "<<head->next->data<<endl;
+//Dynamically 
+
+Node *n3= new node(10);
+Node *head = n3;
+Node *n4= new node(20);
+n3->next=n4;
+*/
+return 0;
+}
